01f2d245d6d536c3c818bad03e475f684d0f5da5
romluc <romulofilhofilho@gmail.com>
2020-05-07 16:39:23 -0300

    Create AuthenticateUserService

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,43 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({
+      where: { email },
+    });
+    if (!user) {
+      throw new Error('Incorrect e-mail/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect e-mail/password combination.');
+    }
+
+    const token = sign({}, '08afd6f9ae0c6017d105b4ce580de885', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
