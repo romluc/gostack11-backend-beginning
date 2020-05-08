37b46026befa26da3565f6fd1aefb0a76853467f
romluc <romulofilhofilho@gmail.com>
2020-05-08 09:46:04 -0300

    Create patch route to upload user avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
-const appointmentsRouter = Router();
+const usersRouter = Router();
+const upload = multer(uploadConfig);
 
-appointmentsRouter.post('/', async (request, response) => {
+usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
 
@@ -24,12 +28,13 @@ appointmentsRouter.post('/', async (request, response) => {
   }
 });
 
-appointmentsRouter.patch(
+usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
+  upload.single('avatar'),
   async (request, response) => {
     return response.json({ ok: true });
   },
 );
 
-export default appointmentsRouter;
+export default usersRouter;
