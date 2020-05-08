21bdf55759c29ed7e3d2634420d0e00e6eb6ee45
romluc <romulofilhofilho@gmail.com>
2020-05-07 21:11:48 -0300

    Add User Authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '08afd6f9ae0c6017d105b4ce580de885',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
+import authConfig from '../config/auth';
 
 interface Request {
   email: string;
@@ -31,9 +32,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect e-mail/password combination.');
     }
 
-    const token = sign({}, '08afd6f9ae0c6017d105b4ce580de885', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
